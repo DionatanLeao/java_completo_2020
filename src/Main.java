82af2cc60d92b8b0233a1f5c97b011754dfcd848
DionatanLeao <dionatan.ti@outlook.com>
2020-12-03 17:39:13 -0300

    Entrada de dados em Java - Parte 2

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		double x = 10.35784;
 		Locale.setDefault(Locale.US);
 		System.out.println(x);
@@ -18,27 +18,42 @@ public class Main {
 		System.out.printf("%.4f%n", x);
 		System.out.println("O valor de x é: " + x);
 		System.out.printf("O valor de x é: %.2f ", x);
-		
+
 		System.out.println("\n---------------------\n");
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		String s;
 		s = sc.next();
 		System.out.println("Você digitou: " + s);
-		
+
 		int i;
 		i = sc.nextInt();
 		System.out.println("Você digitou: " + i);
-		
+
 		double d;
 		d = sc.nextDouble();
 		System.out.println("Você digitou: " + d);
-		sc.close();	
-		
+
 		char c;
 		c = sc.next().charAt(0);
 		System.out.println("Você digitou: " + c);
+
+		int e;
+		String s1, s2, s3;
+		e = sc.nextInt();
+		sc.nextLine();
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+
+		System.out.println("Dados digitados: ");
+		System.out.println(e);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+
+		sc.close();
 	}
 
 }
