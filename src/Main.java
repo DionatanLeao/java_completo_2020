8ad77b522cd66ecdb46e477d39edd124f170a1c5
DionatanLeao <dionatan.ti@outlook.com>
2020-12-02 21:15:54 -0300

    Saida de dados em Java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 /***
  * 
  * @author dionatan
@@ -7,7 +9,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-
+		
+		double x = 10.35784;
+		Locale.setDefault(Locale.US);
+		System.out.println(x);
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f%n", x);
+		System.out.println("O valor de x é: " + x);
+		System.out.printf("O valor de x é: %.2f ", x);
+		
 	}
 
 }
