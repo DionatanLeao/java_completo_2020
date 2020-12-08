a8f09f2e5506638fefd544f47e25040e80789fe3
DionatanLeao <dionatan.ti@outlook.com>
2020-12-08 19:27:04 -0300

    Funções (sintaxe)

--- src/br/com/java/estrutura_repetitivas/Split.java
+++ src/br/com/java/estrutura_repetitivas/Split.java
@@ -7,13 +7,13 @@ package br.com.java.estrutura_repetitivas;
  */
 
 public class Split {
-	
+
 	public static void main(String[] args) {
-		
+
 		String s = "potato apple lemon orange";
-		
+
 		String[] vect = s.split(" ");
-		
+
 		System.out.println(vect[0]);
 		System.out.println(vect[1]);
 		System.out.println(vect[2]);
