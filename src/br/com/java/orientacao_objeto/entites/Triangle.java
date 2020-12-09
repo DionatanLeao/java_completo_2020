5e835119a36a2adec853613463cba7c99ecc89ce
DionatanLeao <dionatan.ti@outlook.com>
2020-12-09 15:38:08 -0300

    Criando um método para obtermos os benefícios de reaproveitamento e delegação

--- src/br/com/java/orientacao_objeto/entites/Triangle.java
+++ src/br/com/java/orientacao_objeto/entites/Triangle.java
@@ -7,8 +7,18 @@ package br.com.java.orientacao_objeto.entites;
  */
 
 public class Triangle {
-	
+
 	public double a;
 	public double b;
 	public double c;
+
+	/**
+	 * Calcula a área do Triangulo
+	 * 
+	 * @return
+	 */
+	public double area() {
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+	}
 }
