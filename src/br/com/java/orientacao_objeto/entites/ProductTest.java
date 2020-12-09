b32e1508525ea81ac8f13e69033c389c0363d6ec
DionatanLeao <dionatan.ti@outlook.com>
2020-12-09 18:02:35 -0300

    Object e toString

--- src/br/com/java/orientacao_objeto/entites/ProductTest.java
+++ src/br/com/java/orientacao_objeto/entites/ProductTest.java
@@ -14,19 +14,19 @@ public class ProductTest {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		Product product = new Product();
 		System.out.println("Enter product data: ");
-		
-		System.out.println("Name: ");
+
+		System.out.print("Name: ");
 		product.name = sc.nextLine();
-		System.out.println("Price: ");
+		System.out.print("Price: ");
 		product.price = sc.nextDouble();
-		System.out.println("Quantity in stock: ");
+		System.out.print("Quantity in stock: ");
 		product.quantity = sc.nextInt();
 		
-		System.out.println(product.name + ", " + product.price + ", " + product.quantity);
-
+		System.out.println(product);
+		
 		sc.close();
 
 	}
