49414a2ee9de938cb5217ad83211f9cfe8c5a7f8
DionatanLeao <dionatan.ti@outlook.com>
2020-12-10 19:39:59 -0300

    Encapsulamento

--- src/br/com/java/orientacao_objeto/entites/Product.java
+++ src/br/com/java/orientacao_objeto/entites/Product.java
@@ -8,9 +8,9 @@ package br.com.java.orientacao_objeto.entites;
 
 public class Product {
 
-	public String name;
-	public double price;
-	public int quantity;
+	private String name;
+	private double price;
+	private int quantity;
 
 	public Product() {
 	}
@@ -24,6 +24,26 @@ public class Product {
 	public Product(String name, double price) {
 		this.name = name;
 		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
 	}
 
 	public double totalValueInStock() {
