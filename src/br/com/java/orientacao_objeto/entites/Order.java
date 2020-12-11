16bd0c025d1119daa0126d8fc8e95cb5356114a9
DionatanLeao <dionatan.ti@outlook.com>
2020-12-11 12:01:49 -0300

    Modificadores de acesso

---
+++ src/br/com/java/orientacao_objeto/entites/Order.java
@@ -0,0 +1,38 @@
+package br.com.java.orientacao_objeto.entites;
+
+import java.util.Date;
+
+/***
+ * 
+ * @author dionatan
+ *
+ */
+
+public class Order {
+
+	private Date date;
+	private Product product;
+
+	public Order(Date date, Product product) {
+		super();
+		this.date = date;
+		this.product = product;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+}
