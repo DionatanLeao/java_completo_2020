a17daf6f271d337b87725079e65a3e91cebc6ac5
DionatanLeao <dionatan.ti@outlook.com>
2020-12-12 13:15:01 -0300

    Correção do exercício de fixação - Parte 2

--- src/br/com/java/orientacao_objeto/entites/Account.java
+++ src/br/com/java/orientacao_objeto/entites/Account.java
@@ -38,13 +38,18 @@ public class Account {
 	public double getBalance() {
 		return balance;
 	}
-	
+
 	public void deposit(double amount) {
 		balance += amount;
 	}
-	
+
 	public void withdraw(double amount) {
 		balance -= amount + 5.0;
+	}
+
+	@Override
+	public String toString() {
+		return "Account " + number + ", Holder: " + holder + ", balance: $ " + String.format("%.2f", balance);
 	}
 
 }
--- src/br/com/java/orientacao_objeto/entites/AccountTest.java
+++ src/br/com/java/orientacao_objeto/entites/AccountTest.java
@@ -1,5 +1,8 @@
 package br.com.java.orientacao_objeto.entites;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /***
  * 
  * @author dionatan
@@ -9,7 +12,45 @@ package br.com.java.orientacao_objeto.entites;
 public class AccountTest {
 
 	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Account account;
+
+		System.out.println("Enter account number: ");
+		int number = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Enter account holder: ");
+		String holder = sc.nextLine();
+		System.out.println("Is there an initial deposit (y/n)? ");
+		char response = sc.next().charAt(0);
+		if (response == 'y') {
+			System.out.println("Enter initial deposit value; ");
+			double initialDeposit = sc.nextDouble();
+			account = new Account(number, holder, initialDeposit);
+		} else {
+			account = new Account(number, holder);
+		}
+
+		System.out.println();
+		System.out.println("Account data: ");
+		System.out.println(account);
 		
+		System.out.println();
+		System.out.print("Enter value deposit: ");
+		double depositValue = sc.nextDouble();
+		account.deposit(depositValue);
+		System.out.println("Update account data: ");
+		System.out.println(account);
+
+		System.out.println();
+		System.out.print("Enter withdraw value: ");
+		double withDrawValue = sc.nextDouble();
+		account.withdraw(withDrawValue);
+		System.out.println("Update account data: ");
+		System.out.println(account);
+		
+		sc.close();
 
 	}
 
