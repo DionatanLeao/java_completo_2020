b394307d16bdb58aa985472cb57ca0d65d158e38
DionatanLeao <dionatan.ti@outlook.com>
2020-12-14 20:30:30 -0300

    Enumerações

---
+++ src/br/com/java/br/com/java/enumeracoes_composicao/OrderStatus.java
@@ -0,0 +1,15 @@
+package br.com.java.br.com.java.enumeracoes_composicao;
+
+/***
+ * 
+ * @author dionatan
+ *
+ */
+
+public enum OrderStatus {
+	
+	PENDING_PAYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED;
+}
---
+++ src/br/com/java/br/com/java/enumeracoes_composicao/OrderTest.java
@@ -0,0 +1,32 @@
+package br.com.java.br.com.java.enumeracoes_composicao;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/***
+ * 
+ * @author dionatan
+ *
+ */
+
+public class OrderTest {
+
+	public static void main(String[] args) {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date d = new Date();
+		String date = sdf.format(d);
+
+		Order order = new Order(1080, date, OrderStatus.PENDING_PAYMENT);
+
+		System.out.println(order);
+
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		System.out.println(os2);
+
+	}
+
+}
