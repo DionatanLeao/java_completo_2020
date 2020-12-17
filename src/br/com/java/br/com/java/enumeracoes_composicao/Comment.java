df9a9dc284e733d5d9d563c03e764fdf5f6f8128
DionatanLeao <dionatan.ti@outlook.com>
2020-12-16 22:03:26 -0300

    Exercício resolvido 2 (demo StringBuilder)

---
+++ src/br/com/java/br/com/java/enumeracoes_composicao/Comment.java
@@ -0,0 +1,28 @@
+package br.com.java.br.com.java.enumeracoes_composicao;
+
+/***
+ * 
+ * @author dionatan
+ *
+ */
+
+public class Comment {
+
+	private String text;
+
+	public Comment() {
+	}
+
+	public Comment(String text) {
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
