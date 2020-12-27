724cde3d9a10c07dbf7dc58ff38815131543857a
DionatanLeao <dionatan.ti@outlook.com>
2020-12-27 18:38:58 -0300

    Exercício resolvido

---
+++ src/br/com/java/br/com/java/heranca_polimorfismo/Employee.java
@@ -0,0 +1,52 @@
+package br.com.java.br.com.java.heranca_polimorfismo;
+
+/***
+ * 
+ * @author dionatan
+ *
+ */
+
+public class Employee {
+
+	private String name;
+	private Integer hours;
+	private Double valuePerHour;
+
+	public Employee() {
+	}
+
+	public Employee(String name, Integer hours, Double valuePerHour) {
+		this.name = name;
+		this.hours = hours;
+		this.valuePerHour = valuePerHour;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getHours() {
+		return hours;
+	}
+
+	public void setHours(Integer hours) {
+		this.hours = hours;
+	}
+
+	public Double getValuePerHours() {
+		return valuePerHour;
+	}
+
+	public void setValuePerHours(Double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+	
+	public double payment() {
+		return hours * valuePerHour;
+	}
+
+}
