d7153e33030ed8a7ca089d5949ef16c751e8fcdf
DionatanLeao <dionatan.ti@outlook.com>
2021-03-10 13:02:47 -0300

    Segunda solução - ruim

--- src/br/com/java/tratamento_excecoes/Program.java
+++ src/br/com/java/tratamento_excecoes/Program.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
@@ -40,16 +40,12 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 
-			Date now = new Date();
+			String error = reservation.updateDates(checkIn, checkOut);
 
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-			} else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
 			} else {
-				reservation.updateDates(checkIn, checkOut);
 				System.out.println("Reservation: " + reservation);
-
 			}
 
 		}
