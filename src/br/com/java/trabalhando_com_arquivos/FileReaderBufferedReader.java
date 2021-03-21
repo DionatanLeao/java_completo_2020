9981d9b0a2f5c265e69e4d73c5c16762233f6099
DionatanLeao <dionatan.ti@outlook.com>
2021-03-21 11:58:21 -0300

    FileReader e BufferedReader

---
+++ src/br/com/java/trabalhando_com_arquivos/FileReaderBufferedReader.java
@@ -0,0 +1,50 @@
+package br.com.java.trabalhando_com_arquivos;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/***
+ * 
+ * @author dionatan
+ *
+ */
+
+public class FileReaderBufferedReader {
+
+	public static void main(String[] args) {
+		
+		String path = "/home/dionatan/workspace/working-files-java/teste.txt";
+		
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+		try {
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			
+			String line = br.readLine();
+			
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		}	catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			try {
+				if(fr != null) {
+					fr.close();
+				}
+				if(br != null) {
+					br.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}		
+
+	}
+
+}
+
