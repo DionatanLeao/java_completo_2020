8baa5b6cc9699bc3fcd89ec834ad88479d50644a
DionatanLeao <dionatan.ti@outlook.com>
2021-04-26 21:18:18 -0300

    Herança múltipla e o problema do diamante - Created class Device

---
+++ src/br/com/java/interfaces/entities/Device.java
@@ -0,0 +1,27 @@
+package br.com.java.interfaces.entities;
+
+/***
+ * 
+ * @author dionatan
+ *
+ */
+
+public abstract class Device {
+
+	private String serialNumber;
+
+	public Device(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+
+	public abstract void processDoc(String doc);
+
+}
