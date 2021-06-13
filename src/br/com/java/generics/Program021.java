d177ea86b8901eeda21881d49f8e8f97390afbb3
DionatanLeao <dionatan.ti@outlook.com>
2021-06-13 11:58:34 -0300

    Criando funções que recebem funções como parâmetro

---
+++ src/br/com/java/generics/Program021.java
@@ -0,0 +1,36 @@
+package br.com.java.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import br.com.java.generics.entities.Product;
+import br.com.java.generics.services.ProductService;
+
+/***
+ * 
+ * @author dionatan
+ *
+ */
+
+public class Program021 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Table", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		ProductService ps = new ProductService();
+		
+		double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'T');
+		
+		System.out.println("Sum = " + String.format("%.2f", sum));
+
+	}
+
+}
---
+++ src/br/com/java/generics/services/ProductService.java
@@ -0,0 +1,25 @@
+package br.com.java.generics.services;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import br.com.java.generics.entities.Product;
+
+/***
+ * 
+ * @author dionatan
+ *
+ */
+
+public class ProductService {
+	
+	public double filteredSum(List<Product> list, Predicate<Product> criteria) {
+		double sum = 0.0;
+		for (Product p : list) {
+			if(criteria.test(p)) {
+				sum += p.getPrice();
+			}
+		}
+		return sum;
+	}
+}
